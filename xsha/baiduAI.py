d84f61f4b98b1bb9342836310f6660194e952f2a
shazhiwei <shazhiwei96@163.com>
2019-08-07 19:04:34 +0800

    baiduAI模块bug修复

--- xsha/baiduAI.py
+++ xsha/baiduAI.py
@@ -12,8 +12,8 @@ class Speech:
     def get_speech(self, text, file=None, lang='zh', ctp=1, spd=5, pit=5, vol=5, per=0):
         """
 
-        :param file: 文件流
         :param text: 合成语音的文本
+        :param file: 文件流
         :param lang: 合成语种
         :param ctp: 默认为1
         :param spd: 语速，取值0-9，默认为5中语速
@@ -25,8 +25,7 @@ class Speech:
         options = {"spd": spd, "pit": pit, "vol": vol, "per": per}
         result = self.client.synthesis(text, lang, ctp, options)
         if file and not isinstance(result, dict):
-            # file.write(result)
-            pass
+            file.write(result)
         else:
             return result
 
@@ -35,5 +34,5 @@ if __name__ == "__main__":
     sp = Speech()
     text = "这是一条测试！"
     with open("audio.mp3", "wb") as f:
-        sp.get_speech(f, text)
+        sp.get_speech(text, file=f)
 
